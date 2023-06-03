3f72d3d9e8ee2bb74cebc45f73c352e0efa0b896
Ridho Nur Cahyo <96169427+RidhoNc11@users.noreply.github.com>
2023-06-03 11:40:06 +0700

    menambahkan footer

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask,redirect,url_for,render_template,request
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def home():
-
     return render_template('landingpage.html')
+
+@app.route("/login")
+def login():
+    return render_template('loginregister.html')
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
