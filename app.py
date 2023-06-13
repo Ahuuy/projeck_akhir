b506e9ff35b230486d34ff107dbbee0ed8a7501f
Ridho Nur Cahyo <96169427+RidhoNc11@users.noreply.github.com>
2023-06-13 17:05:47 +0700

    revisi

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import jwt
 import hashlib
 import os
 import requests
-# import weasyprint
+import weasyprint
 
 app = Flask(__name__)
 
@@ -271,26 +271,26 @@ def verifikasi():
     else:
         return 'Status tidak valid'
     
-# @app.route("/unduh-pdf", methods=['GET'])
-# def unduh_pdf():
-#     # Mendapatkan path direktori "Downloads" pengguna
-#     download_dir = os.path.expanduser("~/Downloads")
+@app.route("/unduh-pdf", methods=['GET'])
+def unduh_pdf():
+    # Mendapatkan path direktori "Downloads" pengguna
+    download_dir = os.path.expanduser("~/Downloads")
 
-#     # Menentukan path lengkap file PDF tujuan
-#     file_path = os.path.join(download_dir, "layout_kartu_ujian.pdf")
+    # Menentukan path lengkap file PDF tujuan
+    file_path = os.path.join(download_dir, "layout_kartu_ujian.pdf")
 
-#     # Render template HTML untuk file "layout_kartu_ujian.html" dengan gambar dari folder "static"
-#     rendered_template = render_template("layout_kartu_ujian.html")
+    # Render template HTML untuk file "layout_kartu_ujian.html" dengan gambar dari folder "static"
+    rendered_template = render_template("layout_kartu_ujian.html")
 
-#     # Konversi HTML menjadi PDF menggunakan WeasyPrint
-#     pdf = weasyprint.HTML(string=rendered_template, base_url=request.host_url).write_pdf()
+    # Konversi HTML menjadi PDF menggunakan WeasyPrint
+    pdf = weasyprint.HTML(string=rendered_template, base_url=request.host_url).write_pdf()
 
-#     # Simpan file PDF ke path tujuan
-#     with open(file_path, 'wb') as file:
-#         file.write(pdf)
+    # Simpan file PDF ke path tujuan
+    with open(file_path, 'wb') as file:
+        file.write(pdf)
 
-#     # Kirim file PDF sebagai respons unduhan
-#     return send_from_directory(directory=download_dir, path="layout_kartu_ujian.pdf", as_attachment=True)
+    # Kirim file PDF sebagai respons unduhan
+    return send_from_directory(directory=download_dir, path="layout_kartu_ujian.pdf", as_attachment=True)
 
     
 @app.route("/unduh_kartu_ujian")
